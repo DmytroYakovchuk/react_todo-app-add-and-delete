48e40a5518a97d902d6e86498fdb33e409b38dbb
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2026-04-30 23:06:00 +0300

    done todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,159 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { ErrorNotification } from './components/ErrorNotification';
+import { TodoList } from './components/TodoList';
+import { Filter } from './api/types/Filters';
+import { TempTodo } from './components/TempTodo';
+import React, { useEffect, useState, useRef } from 'react';
 
-const USER_ID = 0;
+import { Todo } from './api/types/Todo';
+import { addTodo, getTodos, deleteTodo, USER_ID } from './api/todos';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [filter, setFilter] = useState('all');
+  const [newTitle, setNewTitle] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [deletingIds, setDeletingIds] = useState<number[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const showError = (message: string) => {
+    setError(message);
+
+    setTimeout(() => {
+      setError(null);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const trimmedTitle = newTitle.trim();
+
+    if (!trimmedTitle) {
+      showError('Title should not be empty');
+
+      return;
+    }
+
+    setIsAdding(true);
+
+    const newTodo: Todo = {
+      userId: USER_ID,
+      title: trimmedTitle,
+      completed: false,
+    };
+
+    setTempTodo({
+      id: 0,
+      ...newTodo,
+    });
+
+    try {
+      const createdTodo = await addTodo(newTodo);
+
+      setTodos(prev => [...prev, createdTodo]);
+      setNewTitle('');
+    } catch {
+      showError('Unable to add a todo');
+    } finally {
+      setTempTodo(null);
+      setIsAdding(false);
+      inputRef.current?.focus();
+    }
+  };
+
+  const handleDelete = async (id: number) => {
+    try {
+      setDeletingIds(prev => [...prev, id]);
+
+      await deleteTodo(id);
+
+      setTodos(prev => prev.filter(todo => todo.id !== id));
+    } catch {
+      showError('Unable to delete a todo');
+    } finally {
+      setDeletingIds(prev => prev.filter(item => item !== id));
+    }
+  };
+
+  const handleClearCompleted = async () => {
+    const completed = todos.filter(todo => todo.completed);
+
+    try {
+      await Promise.all(completed.map(todo => deleteTodo(todo.id)));
+
+      setTodos(prev => prev.filter(todo => !todo.completed));
+    } catch {
+      showError('Error on delete');
+    }
+  };
+
+  const visibleTodos = todos.filter(todo => {
+    switch (filter) {
+      case Filter.Active:
+        return !todo.completed;
+
+      case Filter.Completed:
+        return todo.completed;
+
+      case Filter.All:
+      default:
+        return true;
+    }
+  });
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          newTitle={newTitle}
+          setNewTitle={setNewTitle}
+          onSubmit={handleSubmit}
+          isAdding={isAdding}
+          allCompleted={todos.every(t => t.completed)}
+        />
+
+        {(todos.length > 0 || tempTodo) && (
+          <section className="todoapp__main" data-cy="TodoList">
+            {tempTodo && <TempTodo todo={tempTodo} />}
+
+            <TodoList
+              todos={visibleTodos}
+              deletingIds={deletingIds}
+              onDelete={handleDelete}
+            />
+          </section>
+        )}
+
+        {todos.length > 0 && (
+          <Footer
+            todos={todos}
+            filter={filter}
+            setFilter={setFilter}
+            onClearCompleted={handleClearCompleted}
+          />
+        )}
+      </div>
+
+      <ErrorNotification error={error} onClose={() => setError(null)} />
+    </div>
   );
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+type Props = {
+  newTitle: string;
+  setNewTitle: (value: string) => void;
+  onSubmit: (e: React.FormEvent) => void;
+  isAdding: boolean;
+  allCompleted: boolean;
+  inputRef: React.RefObject<HTMLInputElement>;
+};
+
+export const Header: React.FC<Props> = ({
+  newTitle,
+  setNewTitle,
+  onSubmit,
+  isAdding,
+  allCompleted,
+}) => {
+  return (
+    <header className="todoapp__header">
+      <button
+        type="button"
+        className={`todoapp__toggle-all ${allCompleted ? 'active' : ''}`}
+        data-cy="ToggleAllButton"
+      />
+
+      <form onSubmit={onSubmit}>
+        <input
+          //ref={inputRef}
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+
+          disabled={isAdding}
+          placeholder="What needs to be done?"
+          className="todoapp__new-todo"
+          data-cy="NewTodoField"
+        />
+      </form>
+    </header>
+  );
+};
